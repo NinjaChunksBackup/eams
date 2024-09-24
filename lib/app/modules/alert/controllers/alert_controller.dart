5371056613ec4abd57e61bfb32c1d5f6161db2d9
azeem30 <pinjariashpak3@gmail.com>
2024-09-24 17:39:03 +0530

    Prevent alerts from stacking

--- lib/app/modules/alert/controllers/alert_controller.dart
+++ lib/app/modules/alert/controllers/alert_controller.dart
@@ -3,6 +3,7 @@ import '../views/alert_view.dart';
 
 class AlertService {
   void raiseAlert(BuildContext context, String message) {
+    if (Navigator.of(context).canPop()) return;
     showDialog(
       context: context,
       builder: (BuildContext context) {
